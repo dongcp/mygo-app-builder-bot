483d73a72cf0c34fea67f284e4140271e93d60a9
dongcp <dongcp.it@gmail.com>
2020-05-18 22:14:04 +0700

    init project

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express();
+
+const TELEGRAM_BASE_URL = "https://api.telegram.org/";
+const TELEGRAM_BOT_TOKEN = "860581299:AAEH_YXKAMACqS3Xx291uwOi75BzXYU6hIs";
+
+const PRIVATE_CHAT_ID = '520305610';
+const GROUP_CHAT_ID = '-1001258719210';
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.post('/start_bot', function(req, res) {
+  const {message} = req.body
+
+  console.log(message)
+});
+
+function sendMessage(content) {
+  axios.post(
+    TELEGRAM_BASE_URL + "/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage",
+    {
+      chat_id: PRIVATE_CHAT_ID,
+      text: content 
+    }
+  ).then (response => {
+      console.log("CheckHang")
+
+  }).catch (error=> {
+    console.log(error)
+  })
+}
+
+app.listen(3000, () => {
+  console.log("Telegram bot is listening on port 3000!")
+});
